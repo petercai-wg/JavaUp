d10375cd26f6d071f17e248856555886765605bd
petercai-wg <petercwg630@gmail.com>
2026-08-12 17:25:57 -0400

    20260812

--- Api-demo/src/test/java/test/JaxbDomUtils.java
+++ Api-demo/src/test/java/test/JaxbDomUtils.java
@@ -1,15 +1,76 @@
 package test;
 
-import jakarta.xml.bind.JAXBContext;
-import jakarta.xml.bind.JAXBException;
-import jakarta.xml.bind.Marshaller;
-import jakarta.xml.bind.Unmarshaller;
+import jakarta.xml.bind.*;
+import org.glassfish.jaxb.runtime.marshaller.NamespacePrefixMapper;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 import javax.xml.transform.dom.DOMResult;
+import java.io.StringWriter;
 
 public class JaxbDomUtils {
+
+    public static String marshal(JAXBElement<?> element) throws Exception {
+
+        JAXBContext context =
+                JAXBContext.newInstance(element.getDeclaredType());
+
+        Marshaller marshaller = context.createMarshaller();
+
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        //JAXB Reference Implementation
+        marshaller.setProperty(
+                "org.glassfish.jaxb.namespacePrefixMapper",
+                new NamespacePrefixMapper() {
+                    @Override
+                    public String getPreferredPrefix(
+                            String namespaceUri,
+                            String suggestion,
+                            boolean requirePrefix) {
+
+                        switch (namespaceUri) {
+                            case "urn:example":
+                                return "";       // default namespace
+
+                            case "urn:other":
+                                return "other";  // keep prefix
+
+                            default:
+                                return suggestion;
+                        }
+
+
+                    }
+                });
+        // JAXB 2.x
+        marshaller.setProperty(
+                "com.sun.xml.bind.namespacePrefixMapper",
+                new NamespacePrefixMapper() {
+                    @Override
+                    public String getPreferredPrefix(
+                            String namespaceUri,
+                            String suggestion,
+                            boolean requirePrefix) {
+
+                        switch (namespaceUri) {
+                            case "urn:example":
+                                return "";       // default namespace
+
+                            case "urn:other":
+                                return "other";  // keep prefix
+
+                            default:
+                                return suggestion;
+                        }
+                    }
+                }
+        );
+        StringWriter writer = new StringWriter();
+        marshaller.marshal(element, writer);
+
+        return writer.toString();
+    }
+
     /**
      * Converts a DOM Element to an object of the specified class.
      *
